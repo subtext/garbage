2a6b6cb768951678e0912a24e97c1d5905b67a1c
Alonzo Turner <aturner@activecampaign.com>
2018-02-06 15:34:46 -0600

    Run ngrok for five minutes to test that it's actually working
    I have a sneaking suspicion that each RUN command in the circle config is self contained. Any processes spawned in a single RUN command will be terminated once that command ends. This should keep the script file alive for five minutes and I can check if ngrok is actually running.

--- ngrok.sh
+++ ngrok.sh
@@ -7,3 +7,7 @@ unzip ngrok-stable-linux-amd64.zip
 sudo mv ngrok /usr/local/bin/ngrok
 ngrok authtoken $NGROK_AUTH_TOKEN
 ngrok tcp 3306 --log=stdout > /dev/null &
+NGROK_STATUS=$(curl http://localhost:4040/api/runnels)
+echo $NGROK_STATUS
+sleep 300
+exit 0
