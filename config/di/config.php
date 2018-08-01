fa1cab4ee2c3c6892602a3435e812fca7eea859a
Alonzo Turner <aturner@mbira.com>
2018-08-01 11:58:34 -0500

    Update DI configuration
    Load the Symfony Request object using the createFromGlobals factory method.
    The Twig loader filesystem is fastest using a relative path and passing the root path as a second parameter.

--- config/di/config.php
+++ config/di/config.php
@@ -3,13 +3,17 @@
 use function DI\create;
 use function DI\get;
 use function DI\string;
+use function DI\factory;
+use Symfony\Component\HttpFoundation\Request;
 
 return [
     'application.root' => dirname(__DIR__, 2),
     'application.locale' => 'en_US',
-    'templates.root' => string('{application.root}/templates'),
+    'templates.root' => 'templates',
+    Request::class => factory([Request::class, 'createFromGlobals']),
     \Twig_Loader_Filesystem::class => create()->constructor(
-        get('templates.root')
+        get('templates.root'),
+        get('application.root')
     ),
     \Twig_Environment::class => create()->constructor(
         get('Twig_Loader_Filesystem')
