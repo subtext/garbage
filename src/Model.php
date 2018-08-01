aef5f5a9cdacff148211b35b585258987fc6edbf
Alonzo Turner <aturner@mbira.com>
2018-08-01 11:56:23 -0500

    Set up default locale for translations

--- src/Model.php
+++ src/Model.php
@@ -4,6 +4,7 @@ namespace Subtext\Garbage;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 class Model
 {
@@ -12,12 +13,31 @@ class Model
      */
     private $request;
 
+    /**
+     * @var Translator
+     */
     private $translator;
+
+    private $languages = ['en_US', 'en_GB', 'en', 'es_ES', 'es', 'fr_FR', 'fr'];
 
     public function __construct(Request $request, Translator $translator)
     {
         $this->request = $request;
         $this->translator = $translator;
+
+        $langs = $this->request->getLanguages();
+        $available = array_intersect($langs, $this->languages);
+        $locale = array_shift($available);
+        $lang = explode('_', $locale)[0];
+        $path = '../i18n/messages.' . $lang . '.yaml';
+
+        // Set the environment locale
+        $this->request->setLocale($locale);
+        $this->translator->setLocale($locale);
+
+        // Load the translations
+        $this->translator->addLoader('yaml', new YamlFileLoader());
+        $this->translator->addResource('yaml', $path, $locale);
     }
 
     /**
